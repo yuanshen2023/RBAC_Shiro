39670c6f6e86237c1a2d525991fc451265321c34
ys <1104357019@qq.com>
2019-12-19 17:12:43 +0800

    测试idea提交代码到GitHub

---
+++ src/main/java/github/mrapee/rbac/common/Test.java
@@ -0,0 +1,14 @@
+package github.mrapee.rbac.common;
+
+/**
+ * @program: rbac_shiro
+ * @description: 测试idea提交代码到github
+ * @author: yuan_shen
+ * @create: 2019-12-19 17:11
+ **/
+
+public class Test {
+    public static void main(String[] args){
+        System.err.println("提交代码到github");
+    }
+}
