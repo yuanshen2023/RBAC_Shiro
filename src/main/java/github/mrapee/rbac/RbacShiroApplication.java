059db1e35202577514bd8840886a1f8094df215a
ys <1104357019@qq.com>
2019-12-19 16:36:59 +0800

    搭建权限管理系统基础环境

---
+++ src/main/java/github/mrapee/rbac/RbacShiroApplication.java
@@ -0,0 +1,13 @@
+package github.mrapee.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacShiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/mrapee/rbac/RbacShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package github.mrapee.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
